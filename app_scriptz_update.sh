d81d60c2bd616e603788c4cac7a03348d976d2b2
tank <tank@certifiedcoderz.com>
2023-12-21 22:45:24 -0500

    Framework Update 2023-12-21@22:45

---
+++ app_scriptz_update.sh
@@ -0,0 +1,23 @@
+# Copyright 2023 Certified CoderZ
+# Author: certifiedcoderz@gmail.com (Certified CoderZ)
+# License GNU General Public License v3.0
+# Description: BiZ9 Framework ScriptZ : App ScriptZ Update
+echo "#################"
+echo "BiZ9 App ScriptZ Update"
+echo "#################"
+G_PROJECT_FOLDER="$HOME/www/projectz/"
+echo "Enter Project ID"
+read project_id
+echo "Enter Directory"
+read folder_id
+G_BIZ_SCRIPT_FOLDER="${BIZ9_HOME}/biz9-scriptz/src/stable"
+G_BIZ_APP_DIR=${G_PROJECT_FOLDER}${project_id}/${folder_id}
+rm -rf ${G_BIZ_APP_DIR}/scriptz
+cp -rf ${G_BIZ_SCRIPT_FOLDER}  ${G_BIZ_APP_DIR}/scriptz
+rm -rf ${G_BIZ_APP_DIR}/scriptz/.biz9_config.sh
+rm -rf ${G_BIZ_APP_DIR}/scriptz/.git
+rm -rf ${G_BIZ_APP_DIR}/scriptz/.gitignore
+echo "----------------------------------"
+echo "Done!"
+echo "----------------------------------"
+exit
