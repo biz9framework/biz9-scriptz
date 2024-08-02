66978e43b1ec62118f03dbee3af0aa4c97b88199
tank <tank@certifiedcoderz.com>
2024-08-01 20:26:54 -0400

    good

---
+++ mobile_android_react_bundle_run.sh
@@ -0,0 +1,21 @@
+# Copyright 2023 Certified CoderZ
+# Author: certifiedcoderz@gmail.com (Certified CoderZ)
+# License GNU General Public License v3.0
+# Description: BiZ9 Framework ScriptZ : Mobile Android React Bundle Run
+source ./.biz9_config.sh
+echo "#################"
+echo "BiZ9 Mobile Android React Bundle Run"
+echo "#################"
+#
+react-native bundle --dev false --platform android --entry-file index.js --bundle-output ./android/app/src/main/assets/index.android.bundle --assets-dest ./android/app/src/main/res
+react-native run-android
+
+fi
+echo "----------------------------------"
+echo "Project-ID: ${PROJECT_ID}"
+echo "App Title: ${APP_TITLE}"
+echo "App-Title-ID: ${APP_TITLE_ID}"
+echo "App Version: ${APP_VERSION}"
+echo "Done!"
+echo "----------------------------------"
+exit 1
