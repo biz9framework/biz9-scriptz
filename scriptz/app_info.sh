77a49ef2fae67bf46a2fd94873917a18071b8830
Certified CoderZ <certifiedcoderz@gmail.com>
2025-01-31 11:49:06 -0500

    good

--- scriptz/app_info.sh
+++ scriptz/app_info.sh
@@ -6,26 +6,24 @@ source biz9_config
 echo "#################"
 echo "BiZ9 Framework App Info"
 echo "#################"
-bash ${BIZ9_SCRIPTZ_DIRECTORY}view_git_sub_header.sh
-echo '------------'
-echo '-- App Info Statt --'
+echo '-------------------------- APP-INFO-STAT -----------------------------------------'
 echo "Title: " ${TITLE};
 echo "Version: " ${VERSION};
 echo "App Title ID: " ${APP_TITLE_ID};
 echo "Project ID: " ${PROJECT_ID};
 echo "Port ID: " ${PORT_ID};
-echo '-- App Info End --'
-echo '-- Git Info Start --'
+echo '--------------------------- APP-INFO-END -----------------------------------------'
+echo '-------------------------- GIT-INFO-START ----------------------------------------'
 echo "Repo: " ${REPO};
 echo "Branch: " ${BRANCH};
-echo '-- Git Info End --'
-echo "-- MongoDB Start --";
+echo '---------------------------- GIT-INFO-END --------------------------------------'
+echo "---------------------------- MONGODB-START -----------------------------------";
 echo "Mongo IP: " ${MONGO_IP};
 echo "Mongo Username Password: " ${MONGO_USERNAME_PASSWORD};
 echo "Mongo Port ID: " ${MONGO_PORT_ID};
 echo "Mongo Server User: " ${MONGO_SERVER_USER};
 echo "Mongo Config File: " ${MONGO_CONFIG_PATH};
 echo "SSH Key: " ${SSH_KEY};
-echo "-- MongoDB End --";
+echo "------------------------------ MONGODB-END --------------------------------------";
 bash ${BIZ9_SCRIPTZ_DIRECTORY}view_footer.sh
 exit
