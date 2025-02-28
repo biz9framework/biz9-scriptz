ff6c04d7099e8d0d1f6ac2a5f36ae2e4b9f1c45e
Certified CoderZ <certifiedcoderz@gmail.com>
2025-02-28 17:16:51 -0500

    good

--- index.js
+++ index.js
@@ -1,16 +1,19 @@
 const fs = require('fs');
 const biz9_config_file=__dirname+"/../../"+"biz9_config";
 
-const get_biz9_config = () => {
-biz9_config = {};
-fileContent = fs.readFileSync(biz9_config_file, 'utf-8');
-    lines = fileContent.split('\n');
-        lines.forEach(line => {
-            const [key, value] = line.split('=');
-            if (key && value) {
-                biz9_config[key] = value.replace(/"/g, '').replace(/'/g, '').replace(/;/g, ''); // Remove quotes
-            }
-        });
+const get_biz9_config =(biz9_config_src) => {
+    if(!biz9_config_src){
+        biz9_config_src=biz9_config_file;
+    }
+    let biz9_config = {};
+    let fileContent = fs.readFileSync(biz9_config_src, 'utf-8');
+    let lines = fileContent.split('\n');
+    lines.forEach(line => {
+        const [key, value] = line.split('=');
+        if (key && value) {
+            biz9_config[key] = value.replace(/"/g, '').replace(/'/g, '').replace(/;/g, ''); // Remove quotes
+        }
+    });
     return biz9_config;
 };
 
