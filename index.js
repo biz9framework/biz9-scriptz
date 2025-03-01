b9ab8fd672e493bcf235a6909376b81cbac61bb9
Certified CoderZ <certifiedcoderz@gmail.com>
2025-03-01 12:26:57 -0500

    good

--- index.js
+++ index.js
@@ -1,23 +1,25 @@
 const fs = require('fs');
 const biz9_config_file=__dirname+"/../../"+"biz9_config";
 
-const get_biz9_config =(biz9_config_src) => {
-    if(!biz9_config_src){
-        biz9_config_src=biz9_config_file;
-    }
-    let biz9_config = {};
-    let fileContent = fs.readFileSync(biz9_config_src, 'utf-8');
-    let lines = fileContent.split('\n');
-    lines.forEach(line => {
-        const [key, value] = line.split('=');
-        if (key && value) {
-            biz9_config[key] = value.replace(/"/g, '').replace(/'/g, '').replace(/;/g, ''); // Remove quotes
+class Config {
+    static get_biz9 =(biz9_config_src) => {
+        if(!biz9_config_src){
+            biz9_config_src=biz9_config_file;
         }
-    });
-    return biz9_config;
-};
+        let biz9_config = {};
+        let fileContent = fs.readFileSync(biz9_config_src, 'utf-8');
+        let lines = fileContent.split('\n');
+        lines.forEach(line => {
+            const [key, value] = line.split('=');
+            if (key && value) {
+                biz9_config[key] = value.replace(/"/g, '').replace(/'/g, '').replace(/;/g, ''); // Remove quotes
+            }
+        });
+        return biz9_config;
+    };
+}
 
 module.exports = {
-    get_biz9_config,
+    Config,
 };
 
--- test.js
+++ test.js
@@ -1,9 +1,9 @@
-import { get_biz9_config } from "./";
+import { Config } from "./";
 const BIZ9_SRC_FILE = "./biz9_config";
 test('connect', async () => {
     console.log('GET-BIZ9-File-START');
-    let biz9_config_file = get_biz9_config(BIZ9_SRC_FILE);
+    let biz9_config_file = Config.get_biz9(BIZ9_SRC_FILE);
     console.log('biz9_config_file',biz9_config_file);
-     console.log('GET-BIZ9-File-SUCCESS');
+    console.log('GET-BIZ9-File-SUCCESS');
 });
 
