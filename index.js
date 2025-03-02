c401fded3dca8e4ac310a2e6bc250abc24c4f4fe
Certified CoderZ <certifiedcoderz@gmail.com>
2025-03-01 19:49:42 -0500

    good

--- index.js
+++ index.js
@@ -1,22 +1,28 @@
 const fs = require('fs');
-const biz9_config_file=__dirname+"/../../"+"biz9_config";
+const biz9_config_local=__dirname+"/../../"+"biz9_config";
 
 class Scriptz {
     static get_biz9_config =(biz9_config_src) => {
         if(!biz9_config_src){
-            biz9_config_src=biz9_config_file;
+            biz9_config_src=biz9_config_local;
         }
-        let biz9_config = {};
-        let fileContent = fs.readFileSync(biz9_config_src, 'utf-8');
-        let lines = fileContent.split('\n');
-        lines.forEach(line => {
-            const [key, value] = line.split('=');
-            if (key && value) {
-                biz9_config[key] = value.replace(/"/g, '').replace(/'/g, '').replace(/;/g, ''); // Remove quotes
-            }
-        });
-        return biz9_config;
-    };
+        if (!fs.existsSync(biz9_config_src)) {
+            console.log('biz9_config_file',biz9_config_local);
+            console.log('File does not exist');
+            console.log('BiZ9 Config File. No Source Availble.');
+        } else {
+            let biz9_config = {};
+            let fileContent = fs.readFileSync(biz9_config_src, 'utf-8');
+            let lines = fileContent.split('\n');
+            lines.forEach(line => {
+                const [key, value] = line.split('=');
+                if (key && value) {
+                    biz9_config[key] = value.replace(/"/g, '').replace(/'/g, '').replace(/;/g, ''); // Remove quotes
+                }
+            });
+            return biz9_config;
+        }
+    }
 }
 
 module.exports = {
--- test.js
+++ test.js
@@ -3,6 +3,7 @@ const BIZ9_SRC_FILE = "./biz9_config";
 test('connect', async () => {
     console.log('GET-BIZ9-File-START');
     let biz9_config_file = Scriptz.get_biz9_config(BIZ9_SRC_FILE);
+    //let biz9_config_file = Scriptz.get_biz9_config();
     console.log('biz9_config_file',biz9_config_file);
     console.log('GET-BIZ9-File-SUCCESS');
 });
