cb50f07a6f347f5e70394c065e1c88eef6eb4848
Certified CoderZ <certifiedcoderz@gmail.com>
2025-06-08 17:53:12 -0400

    good

--- scriptz/project_framework_update.sh
+++ scriptz/project_framework_update.sh
@@ -17,6 +17,7 @@ if [[  "$n" = "yes"  ]] ; then
     #DEST_PROJECT_TITLE="service-node";
     DEST_PROJECT_DIR=${BIZ9_PROJECT_DIRECTORY}${DEST_PROJECT_ID}
     DEST_PROJECT_CODE=${BIZ9_PROJECT_DIRECTORY}${DEST_PROJECT_ID}/code/${DEST_PROJECT_TITLE}
+    BK_DATE=$(date +%Y%m%d_%H%M);
 
     SRC_FRAMEWORK_DIR=${BIZ9_HOME}${BIZ9_PRODUCT_TITLE}/code
 
@@ -24,12 +25,16 @@ if [[  "$n" = "yes"  ]] ; then
     if [ ! -d "${DEST_PROJECT_DIR}/backup" ]; then
         mkdir ${DEST_PROJECT_DIR}/backup
     fi
-    cp -rf ${DEST_PROJECT_CODE} ${DEST_PROJECT_DIR}/backup/${DEST_PROJECT_TITLE}_$(date +%Y%m%d_%H%M)
+    cp -rf ${DEST_PROJECT_CODE} ${DEST_PROJECT_DIR}/backup/${DEST_PROJECT_TITLE}_${BK_DATE}
     # bk org project -- end
 
     # cp framework src to project dir -- start
     cp -rf ${SRC_FRAMEWORK_DIR}/* ${DEST_PROJECT_CODE}
     # cp framework src to project dir -- end
+
+    #copy biz9_config back -- start
+    cp -rf ${DEST_PROJECT_DIR}/backup/${DEST_PROJECT_TITLE}_${BK_DATE}/biz9_config ${DEST_PROJECT_CODE}/
+    #copy biz9_config back -- end
 else
     echo 'exit success';
     exit
